865510cc2cd33abdb5a7f3c3171a5a3faed77966
WavDASH <alexwu0726@163.com>
2023-10-15 14:23:08 +0800

    initial
    initial

---
+++ src/autonomous.cpp
@@ -0,0 +1,174 @@
+#include "autonomous.h"
+#include "parameters.h"
+#include "my-timer.h"
+#include "robot-config.h"
+#include "GPS.h"
+#include "ezize.h"
+
+void autonInit(void) {
+  setPistonE1(false);
+  setPistonE2(false);
+  resetHeading();
+  resetForwardPos();
+  MyGps.resetForwardPosGps();
+}
+
+
+void auton_near_1(){
+  MyTimer autotimer;
+  autotimer.reset();
+  setIntakeSpeed(100);
+  setCataStatus(5, 0);
+  softStartTimerForward(0, 80, 200);
+  posForwardAbsWithHeading(80, 1100, 0);
+  PIDPosForwardAbs(1280);
+  PIDPosForwardAbs(670);
+  PIDAngleRotateAbs(-135, 2.5, 0.03, 5, 1.5);
+  // this_thread::sleep_for(50);
+  setCataStatus(2);
+  this_thread::sleep_for(300);
+
+  setIntakeSpeed(0);
+  PIDAngleRotateAbs(-150);
+  setCataStatus(5);
+  PIDPosForwardAbs(830);
+  setPistonHook(1);
+  voltageForward(15);
+  this_thread::sleep_for(400);
+
+  resetForwardPos();
+  PIDPosForwardAbs(-500);
+  this_thread::sleep_for(100);
+  resetForwardPos();
+  PIDPosCurveAbs(1, 800);
+  setPistonHook(0);
+  setIntakeSpeed(-100);
+  PIDPosForwardAbs(800);
+  setIntakeSpeed(0);
+
+  Brain.Screen.setCursor(8, 1);
+  Brain.Screen.print("Auton Time: %.1f                 ", autotimer.getTime());
+}
+
+void auton_near_2(){
+  MyTimer autotimer;
+  autotimer.reset( );
+  setIntakeSpeed(0);
+  setPistonHook(1);
+  this_thread::sleep_for(300);
+  PIDAngleRotateAbs(-90, 1.1, 0.05, 5);
+  setPistonHook(0);
+
+  PIDPosForwardAbs(630);
+  //PIDPosCurveAbs(500,300,5);
+  PIDAngleRotateAbs(-45, 0.8, 0.05, 5);
+  setIntakeSpeed(-100);
+  voltageForward(15);
+  this_thread::sleep_for(300);
+  voltageForward(70);
+  this_thread::sleep_for(400);
+
+  setIntakeSpeed(0);
+  setCataStatus(5,0);
+  PIDPosForwardAbs(0);
+  PIDAngleRotateAbs(-90,0.8,0.05,5);
+  PIDPosForwardAbs(-550);
+  PIDAngleRotateAbs(-135,0.8,0.05,5);
+  PIDPosForwardAbs(-950);
+  //PIDPosCurveAbs(-1600,-1100,5);
+  
+  this_thread::sleep_for(100);
+
+
+  Brain.Screen.setCursor(8, 1);
+  Brain.Screen.print("Auton Time: %.1f                 ", autotimer.getTime());
+}
+
+void auton_far_1(){
+  MyTimer autotimer;
+  autotimer.reset();
+  setIntakeSpeed(100);//
+  setCataStatus(5, 1);
+  softStartTimerForward(0, 80, 200);
+  posForwardAbsWithHeading(80, 1100, 0);
+  PIDPosForwardAbs(1400);
+  PIDAngleRotateAbs(105, 2, 0.05, 5);
+  setIntakeSpeed(0);
+  posForwardAbsWithHeading(80, 150, 105);
+  setIntakeSpeed(-100);
+  voltageForward(10);
+  this_thread::sleep_for(300);
+  voltageForward(60);
+  this_thread::sleep_for(300);
+
+  PIDPosForwardAbs(0);
+  PIDAngleRotateAbs(-75, 1.5, 0.05, 10);
+  setIntakeSpeed(100);
+  //PIDPosForwardAbs(330);
+  voltageForward(50);
+  this_thread::sleep_for(600);
+  PIDPosForwardAbs(0);
+  setIntakeSpeed(0);
+  PIDAngleRotateAbs(100, 1.5, 0.05, 10);
+  posForwardAbsWithHeading(80, 150, 100);
+  setIntakeSpeed(-100);
+  voltageForward(20);
+  this_thread::sleep_for(350);
+  voltageForward(70);
+  this_thread::sleep_for(300);
+
+  PIDPosForwardAbs(0);
+  PIDAngleRotateAbs(-165, 1.5, 0.05, 10);
+  PIDPosForwardAbs(550);
+  PIDAngleRotateAbs(-75, 1.5, 0.05, 10);
+  setIntakeSpeed(100);
+  //PIDPosForwardAbs(350);
+  voltageForward(50);
+  this_thread::sleep_for(620);
+  PIDPosForwardAbs(10);
+  setIntakeSpeed(0);
+  PIDAngleRotateAbs(65, 1.5, 0.05, 10);
+  posForwardAbsWithHeading(80, 150, 65);
+  setIntakeSpeed(-100);
+  voltageForward(20);
+  this_thread::sleep_for(450);
+  voltageForward(70);
+  this_thread::sleep_for(400);
+  PIDPosForwardAbs(0);
+  setIntakeSpeed(0);
+  //setPistonE(1);
+
+  Brain.Screen.setCursor(8, 1);
+  Brain.Screen.print("Auton Time: %.1f                 ", autotimer.getTime());
+}
+
+void auton_far_2(){
+  MyTimer autotimer;
+  autotimer.reset();
+
+  Brain.Screen.setCursor(8, 1);
+  Brain.Screen.print("Auton Time: %.1f                 ", autotimer.getTime());
+}
+
+void test(){
+  MyTimer autotimer;
+  autotimer.reset();
+
+  Brain.Screen.setCursor(8, 1);
+  Brain.Screen.print("Auton Time: %.1f                 ", autotimer.getTime());
+}
+
+
+// #ifdef ROBOT1
+void runAuton(int auton_choose) {
+  
+  setAutonMode();
+  autonFlipper(true);
+  autonInit();
+
+  if (auton_choose == 1) auton_near_1();        // 1+2 near
+  else if (auton_choose == 2) auton_near_2();   //
+  else if (auton_choose == 3) auton_far_1();    //
+  else if (auton_choose == 4) auton_far_2();
+}
+// #endif
---
+++ src/main.cpp
@@ -0,0 +1,216 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Competition Template                                      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Motor18              motor         18              
+// ---- END VEXCODE CONFIGURED DEVICES ----
+#include "ezize.h"
+#include "basic-functions.h"
+#include "parameters.h"
+#include "autonomous.h"
+#include "my-timer.h"
+#include "iostream"
+#include "GPS.h"
+#include "skill.h"
+
+
+using namespace std;
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+int auton_choose = 3;
+bool autolowlift = 0;
+bool firstTime = 1;
+
+
+void autonomous(void) {
+  runAuton(auton_choose);
+  // runSkill();
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+
+void usercontrol(void) {
+
+  int print_i = 0;
+  int autocollide = 0;
+  bool RightPressed = 0;
+  bool LEFTPressed = 0;
+  bool BAPressed = 0;
+  bool L2Pressed = 0;
+  bool BXPressed = 0;
+  bool extensionStatus = 0;
+  float m_degree = Motor_Cata1.position(deg);
+
+  while(true) {
+
+    int Ch1 = abbs(C1) < Joystick_LowerDeadzone ? 0 : C1;
+    int Ch2 = abbs(C2) < Joystick_LowerDeadzone ? 0 : C2;
+    int Ch3 = abbs(C3) < Joystick_LowerDeadzone ? 0 : C3;
+    // int Ch4 = abbs(C4) < Joystick_LowerDeadzone ? 0 : C4;
+    if (sign(Ch3) == sign(Ch2) && abbs(Ch2) > 80 && abbs(Ch3) > 80 && abbs(Ch2) > (abbs(Ch1) + 40)) autocollide = sign(Ch2);
+    else autocollide = 0;
+    
+    if(LEFT && !LEFTPressed){  // Auto low lift
+      if (firstTime){  // First time, only close the hook
+        setPistonE1(1);
+        firstTime = !firstTime;
+      }
+      else {
+        autolowlift = !autolowlift;
+        clearLowLiftStep();
+        setPistonE1(!autolowlift);
+      }
+    }
+    LEFTPressed = LEFT;
+    
+    if (autolowlift == 1) autoLowLift();
+    else if (autocollide == 0) {
+      //unlockBase();
+      moveLeft(Ch3 + 1* Ch1);
+      moveRight(Ch3 - 1* Ch1);
+    } 
+    else {
+      moveLeft(autocollide * 15);
+      moveRight(autocollide * 15);
+    }
+        
+    if (R1) setIntakeSpeed(100);
+    else if (R2) setIntakeSpeed(-100);
+    else setIntakeSpeed(0);
+
+    if (BY) setCataStatus(4);
+    if (L1) setCataStatus(2);
+    if (L2 && !L2Pressed) setCataStatus(5);
+    L2Pressed = L2;
+    if (BX && !BXPressed) setCataStatus(6);
+    BXPressed = BX;
+
+    if (LEFT && !LEFTPressed && BA && !BAPressed){
+      extensionStatus = !extensionStatus;
+      setPistonE1(extensionStatus);
+      setPistonE2(extensionStatus);
+    }
+    else if (LEFT && !LEFTPressed){
+      extensionStatus = !extensionStatus;
+      setPistonE1(extensionStatus);
+    }
+    else if (BA && !BAPressed){
+      extensionStatus = !extensionStatus;
+      setPistonE2(extensionStatus);
+    }
+    BAPressed = BA;
+    LEFTPressed = LEFT;
+
+    // only when down and R2 are both pressed we run auton
+    if (DOWN) runAuton(auton_choose);
+    if (DOWN && R2) runSkill();
+
+    if (RIGHT && !RightPressed) auton_choose = ((auton_choose + 1) - 1) % 4 + 1;
+    RightPressed = RIGHT;
+
+    m_degree = Motor_Cata1.position(deg) / 3;
+
+    if (print_i == 0){
+      // Brain.Screen.clearScreen();
+      Brain.Screen.setCursor(1, 1);
+      Brain.Screen.print("Heading: %.1f                             ", getHeading());
+    
+      Brain.Screen.setCursor(2, 1);
+      Brain.Screen.print("ForwardPosition: %.1f                     ", getForwardPos());
+
+      Brain.Screen.setCursor(7, 1);
+      Brain.Screen.print("Auton choose: %d                          ", auton_choose);
+
+      Brain.Screen.setCursor(5, 1);
+      Brain.Screen.print("m_degree: %.1f                     ", m_degree);
+    }
+    print_i += 1;
+    print_i %= 100;
+
+    this_thread::sleep_for(5);
+    
+  }
+}
+
+
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  wait(1000, msec);
+  setPistonE1(false);
+  setPistonE2(false);
+  IMU.startCalibration();
+  while (IMU.isCalibrating()) {
+  }
+  wait(1000, msec);
+
+  thread Intake(intake);
+  thread Catapult(catapult);
+  thread GPSPosition(MyGpsPos);
+  
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) wait(5, msec);
+}
